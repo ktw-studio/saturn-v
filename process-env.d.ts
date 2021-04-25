1b06607e5cd78224659db33c8eb3a44926e99d6e
Marcin Mazurowski @ _KTW-STUDIO <hello@ktwstudio.com>
2021-04-25 23:00:30 +0200

    :sparkles: Docker init

--- process-env.d.ts
+++ process-env.d.ts
@@ -3,6 +3,15 @@ export interface ProcessEnvironments {
     APP_HOST: string;
     APP_PORT: number;
     LOG_LEVEL: 'error'|'debug' | 'log' | 'warn' | 'verbose' | 'info';
+
+    POSTGRES_PORT: number;
+    POSTGRES_DATABASE_NAME:string;
+    POSTGRES_USER:string;
+    POSTGRES_PASSWORD:string;
+
+    MONGO_PORT:number;
+    MONGO_USER:string;
+    MONGO_PASSWORD:string;
 }
 
 declare namespace NodeJS {
